73cc933f1e5dc83bcb5ec05862369f06438030c9
Junior <mjrjunior4@gmail.com>
2024-05-31 16:51:56 -0300

    completion of the create order page

--- src/pages/app/orders/order-create.tsx
+++ src/pages/app/orders/order-create.tsx
@@ -8,30 +8,66 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@radix-ui/react-label'
 import { Button } from '@/components/ui/button'
 
-import { SetStateAction, useState } from 'react'
+import { useEffect, useState } from 'react'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+import { Trash2 } from 'lucide-react'
+
+type Product = {
+  id: number
+  name: string
+  price: number
+} | null
 
 const products = [
-  { id: 1, name: 'Pizza de Calabresa', price: 29.9 },
-  { id: 2, name: 'Pizza de Frango', price: 29.9 },
-  { id: 3, name: 'Pizza de Marguerita', price: 29.9 },
-  { id: 4, name: 'Pizza de Mussarela', price: 29.9 },
-  { id: 5, name: 'Pizza de Pepperoni', price: 29.9 },
+  { id: 1, name: 'Malinha', price: 29.9 },
+  { id: 2, name: 'Necessarie M', price: 29.9 },
+  { id: 3, name: 'Porta Make P', price: 29.9 },
+  { id: 4, name: 'Térmica Trapézio', price: 29.9 },
+  { id: 5, name: 'Estojo Cristal', price: 29.9 },
 ]
 
 export function OrderCreate() {
-  const [selectedProduct, setSelectedProduct] = useState('')
-  const [selectedProducts, setSelectedProducts] = useState<string[]>([])
+  const [selectedProduct, setSelectedProduct] = useState<Product>(null)
+  const [selectedProducts, setSelectedProducts] = useState<Product[]>([])
+  const [inputValues, setInputValues] = useState<number[]>([])
+  const [total, setTotal] = useState(0)
 
-  const handleSelectChange = (event: {
-    target: { value: SetStateAction<string> }
-  }) => {
-    setSelectedProduct(event.target.value)
+  const handleProductChange = (event: { target: { value: unknown } }) => {
+    const productId = Number(event.target.value)
+    const product = products.find((product) => product.id === productId)
+    if (product) {
+      setSelectedProduct(product)
+      setInputValues([...inputValues, 1])
+    }
   }
+
+  useEffect(() => {
+    const newTotal = selectedProducts.reduce((acc, product, index) => {
+      if (product) {
+        return acc + product.price * inputValues[index]
+      } else {
+        return acc
+      }
+    }, 0)
+    setTotal(newTotal)
+  }, [inputValues, selectedProducts])
 
   const handleAddProduct = (event: { preventDefault: () => void }) => {
     event.preventDefault()
-    setSelectedProducts([...selectedProducts, selectedProduct])
-    setSelectedProduct('')
+    if (selectedProduct) {
+      setSelectedProducts([...selectedProducts, selectedProduct])
+      setSelectedProduct(null)
+      console.log(selectedProducts)
+      console.log(total)
+    }
   }
 
   return (
@@ -49,20 +85,15 @@ export function OrderCreate() {
         <Input type="tel" id="phone" placeholder="Telefone" />
         <div className="w-180 flex h-8 gap-3">
           <select
-            value={selectedProduct}
-            onChange={handleSelectChange}
-            defaultValue=""
+            value={selectedProduct ? selectedProduct?.id : ''}
+            onChange={handleProductChange}
             className="h-8 w-[180px] rounded-md border border-input bg-secondary text-center text-sm font-semibold"
           >
             <option disabled value="">
               Selecione um produto
             </option>
             {products.map((product) => (
-              <option
-                key={product.id}
-                value={product.name}
-                className="text-left"
-              >
+              <option key={product.id} value={product.id} className="text-left">
                 {product.name}
               </option>
             ))}
@@ -75,9 +106,53 @@ export function OrderCreate() {
             Adicionar
           </Button>
         </div>
-        {selectedProducts.map((product, index) => (
-          <p key={index}>{product}</p>
-        ))}
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Produto</TableHead>
+              <TableHead>Preço</TableHead>
+              <TableHead>Qtd.</TableHead>
+              <TableHead> </TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {selectedProducts.map((product, index) => (
+              <TableRow key={index}>
+                <TableCell>{product?.name}</TableCell>
+                <TableCell>R$ {product?.price.toFixed(2)}</TableCell>
+                <TableCell>
+                  <Input
+                    value={inputValues[index]}
+                    onChange={(e) => {
+                      const newInputValues = [...inputValues]
+                      newInputValues[index] = Number(e.target.value)
+                      setInputValues(newInputValues)
+                    }}
+                    min="1"
+                    type="number"
+                    className="w-14 text-center no-spin"
+                  />
+                </TableCell>
+                <TableCell>
+                  <Button variant="destructive">
+                    <Trash2 size={16} />
+                  </Button>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TableCell colSpan={3}>Total do pedido</TableCell>
+              <TableCell className="text-right font-medium">
+                R$ {total.toFixed(2)}
+              </TableCell>
+            </TableRow>
+          </TableFooter>
+        </Table>
+        <Button variant="secondary" type="submit">
+          Criar Pedido
+        </Button>
       </form>
     </DialogContent>
   )
